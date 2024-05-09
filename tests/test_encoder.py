fafea8766b32d8faa9d80e16b1d0a32b44441292
jpan326 <jpan326@gatech.edu>
2024-05-09 14:30:54 +0800

    add test_encoder

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -12,6 +12,7 @@ def test_get_bits():
     assert result == "b0df12f"
     encoder.get_bits.assert_called_once_with("www5")
 
+
 def test_check_collision():
     db_accessor_mock = Mock()
     db_accessor_mock.longurl_exists.return_value = False
@@ -19,9 +20,10 @@ def test_check_collision():
 
     encoder = Encoder(db_accessor_mock)
     encoder.check_collision = Mock(return_value=False)
-    result = encoder.check_collision("www5","b0df12f")
+    result = encoder.check_collision("www5", "b0df12f")
     assert result == False
-    encoder.check_collision.assert_called_once_with("www5","b0df12f")
+    encoder.check_collision.assert_called_once_with("www5", "b0df12f")
+
 
 def test_get_encoding():
     db_accessor_mock = Mock()
